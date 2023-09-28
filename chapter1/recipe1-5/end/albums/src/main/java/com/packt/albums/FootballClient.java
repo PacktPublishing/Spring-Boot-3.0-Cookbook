cb89790feb3d357c8dd4a1181bf4f8d2040a41d6
felipmiguel <52053185+felipmiguel@users.noreply.github.com>
2023-09-29 00:58:43 +0200

    recipe 1-5

--- chapter1/recipe1-5/end/albums/src/main/java/com/packt/albums/FootballClient.java
+++ chapter1/recipe1-5/end/albums/src/main/java/com/packt/albums/FootballClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "football", url = "http://localhost:8080")
+@FeignClient("FootballServer")
 public interface FootballClient {
 
     @RequestMapping(method = RequestMethod.GET, value = "/players")
