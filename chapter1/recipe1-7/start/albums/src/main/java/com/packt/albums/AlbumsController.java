6f307c547ef24363461a3b2da430afb0913d5a08
felipmiguel <52053185+felipmiguel@users.noreply.github.com>
2023-09-29 01:09:48 +0200

    recipe 1-7

---
+++ chapter1/recipe1-7/start/albums/src/main/java/com/packt/albums/AlbumsController.java
@@ -0,0 +1,27 @@
+package com.packt.albums;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/albums")
+public class AlbumsController {
+    private final FootballClient footballClient;
+
+    public AlbumsController(FootballClient footballClient) {
+        this.footballClient = footballClient;
+    }
+
+    @GetMapping("/players")
+    public List<Player> getPlayers() {
+        return footballClient.getPlayers();
+    }
+
+    @GetMapping("/serviceinfo")
+    public String getServiceInfo() {
+        return footballClient.getServiceInfo();
+    }
+}
