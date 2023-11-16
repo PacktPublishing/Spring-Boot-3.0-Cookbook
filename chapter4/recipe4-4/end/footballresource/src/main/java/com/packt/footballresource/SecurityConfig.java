0c6781dfc88574fc7659088280cb6e9a4302e5b3
felipmiguel <52053185+felipmiguel@users.noreply.github.com>
2023-11-16 23:32:05 +0100

    recipe4-4 end

--- chapter4/recipe4-4/end/footballresource/src/main/java/com/packt/footballresource/SecurityConfig.java
+++ chapter4/recipe4-4/end/footballresource/src/main/java/com/packt/footballresource/SecurityConfig.java
@@ -20,13 +20,13 @@ public class SecurityConfig {
                 .build();
     }
 
-    @Bean
-    public SecurityFilterChain filterChainRoles(HttpSecurity http) throws Exception {
-        return http
-                .authorizeHttpRequests(
-                        authorize -> authorize.requestMatchers(HttpMethod.POST, "football/teams/**").hasRole("ADMIN")
-                                .anyRequest().authenticated())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
-                .build();
-    }
+//     @Bean
+//     public SecurityFilterChain filterChainRoles(HttpSecurity http) throws Exception {
+//         return http
+//                 .authorizeHttpRequests(
+//                         authorize -> authorize.requestMatchers(HttpMethod.POST, "football/teams/**").hasRole("ADMIN")
+//                                 .anyRequest().authenticated())
+//                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+//                 .build();
+//     }
 }
