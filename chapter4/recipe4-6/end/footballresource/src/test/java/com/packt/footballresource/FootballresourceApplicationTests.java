7e9cf18e47cb0cb1365b5bd61dfcba47052e9c75
felipmiguel <52053185+felipmiguel@users.noreply.github.com>
2023-11-23 22:35:35 +0100

    recipe4-6 start

---
+++ chapter4/recipe4-6/end/footballresource/src/test/java/com/packt/footballresource/FootballresourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.packt.footballresource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballresourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ chapter4/recipe4-6/start/registry/src/main/java/com/packt/registry/RegistryApplication.java
@@ -0,0 +1,15 @@
+package com.packt.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class RegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistryApplication.class, args);
+	}
+
+}
