43c0ea557908c607a46f6b715336dd590921475c
felipmiguel <52053185+felipmiguel@users.noreply.github.com>
2023-11-29 00:28:37 +0100

    reorder chapter 4 recipes

--- chapter4/recipe4-6/end/footballui/src/main/java/com/packt/footballui/controller/FootballController.java
+++ chapter4/recipe4-6/end/footballui/src/main/java/com/packt/footballui/controller/FootballController.java
@@ -4,9 +4,13 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -32,16 +36,18 @@ public class FootballController {
     }
 
     @GetMapping("/teams")
-    public String teams(@RegisteredOAuth2AuthorizedClient("football-ui") OAuth2AuthorizedClient authorizedClient,
+    public String teams(@RegisteredOAuth2AuthorizedClient("football-api") OAuth2AuthorizedClient authorizedClient,
             Model model) {
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + authorizedClient.getAccessToken().getTokenValue());
+        headers.add(HttpHeaders.AUTHORIZATION, "Bearer " +
+                authorizedClient.getAccessToken().getTokenValue());
 
         HttpEntity<String> entity = new HttpEntity<>(null, headers);
 
         ResponseEntity<String> response = restTemplate.exchange(
-                "http://localhost:8080/football/teams", HttpMethod.GET, entity, String.class);
+                "http://localhost:8080/football/teams", HttpMethod.GET, entity,
+                String.class);
         model.addAttribute("teams", response.getBody());
         return "teams";
     }
