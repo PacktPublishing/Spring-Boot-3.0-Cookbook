930bf0b8bc11b59e8ad0675b1c2cf973d20ef13f
felipmiguel <52053185+felipmiguel@users.noreply.github.com>
2024-02-10 00:23:00 +0100

    fix recipe 6-1 mongo initialization

---
+++ chapter6/recipe6-2/end/footballmdb/src/test/java/com/packt/footballmdb/service/FootballServiceTest.java
@@ -0,0 +1,39 @@
+package com.packt.footballmdb.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class FootballServiceTest {
+
+    @Test
+    void getTeam() {
+    }
+
+    @Test
+    void getTeamByName() {
+    }
+
+    @Test
+    void getTeamsContainingName() {
+    }
+
+    @Test
+    void getPlayer() {
+    }
+
+    @Test
+    void getTeamByNameSQL() {
+    }
+
+    @Test
+    void saveTeam() {
+    }
+
+    @Test
+    void deleteTeam() {
+    }
+
+    @Test
+    void updateTeamName() {
+    }
+}
