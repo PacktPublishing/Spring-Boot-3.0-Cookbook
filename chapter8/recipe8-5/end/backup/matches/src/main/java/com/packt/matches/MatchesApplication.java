9a2561f14fc5c32e31f74f6fbadda3207782b7c5
felipmiguel <52053185+felipmiguel@users.noreply.github.com>
2024-03-18 00:19:32 +0100

    backup (not working)

---
+++ chapter8/recipe8-5/end/backup/matches/src/main/java/com/packt/matches/MatchesApplication.java
@@ -0,0 +1,13 @@
+package com.packt.matches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchesApplication.class, args);
+	}
+
+}
---
+++ chapter8/recipe8-5/end/backup/timeline/src/test/java/com/packt/timeline/TimelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.packt.timeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
