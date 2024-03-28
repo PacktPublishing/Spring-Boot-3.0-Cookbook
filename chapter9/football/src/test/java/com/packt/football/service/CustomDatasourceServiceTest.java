a0abc8d2020c5b6748e5fd5a403008a8cb935a09
felipmiguel <52053185+felipmiguel@users.noreply.github.com>
2024-03-28 22:03:43 +0100

    sample spring boot 2.6

---
+++ chapter9/football/src/test/java/com/packt/football/service/CustomDatasourceServiceTest.java
@@ -0,0 +1,54 @@
+package com.packt.football.service;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.DataSourceInitializationMode;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.sql.Date;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Testcontainers
+@ContextConfiguration(initializers = CustomDatasourceServiceTest.Initializer.class)
+class CustomDatasourceServiceTest {
+
+    static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:latest")
+            .withDatabaseName("football")
+            .withUsername("football")
+            .withPassword("football");
+
+    static class Initializer
+            implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+            TestPropertyValues.of(
+                            "spring.datasource.url=" + postgreSQLContainer.getJdbcUrl(),
+                            "spring.datasource.username=" + postgreSQLContainer.getUsername(),
+                            "spring.datasource.password=" + postgreSQLContainer.getPassword())
+                    .applyTo(configurableApplicationContext.getEnvironment());
+        }
+    }
+
+    @BeforeAll
+    public static void startContainer() {
+        postgreSQLContainer.start();
+    }
+
+    @Autowired
+    CustomDatasourceService customDatasourceService;
+
+    @Test
+    void getInitializationMode() throws SQLException {
+        DataSourceInitializationMode initializationMode = customDatasourceService.getInitializationMode();
+        assertEquals(DataSourceInitializationMode.EMBEDDED, initializationMode);
+    }
+}
