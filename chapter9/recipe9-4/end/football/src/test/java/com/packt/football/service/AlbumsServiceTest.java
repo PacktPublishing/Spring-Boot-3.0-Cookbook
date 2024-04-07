61937e52fd980e2595a8356ee5ebd0f8bde4af20
felipmiguel <52053185+felipmiguel@users.noreply.github.com>
2024-04-08 01:23:15 +0200

    recipe 9-4 end

--- chapter9/recipe9-4/end/football/src/test/java/com/packt/football/service/AlbumsServiceTest.java
+++ chapter9/recipe9-4/end/football/src/test/java/com/packt/football/service/AlbumsServiceTest.java
@@ -19,7 +19,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.CassandraContainer;
@@ -53,10 +52,10 @@ class AlbumsServiceTest {
 
     @DynamicPropertySource
     static void setCassandraProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.data.cassandra.keyspace-name", () -> "footballKeyspace");
-        registry.add("spring.data.cassandra.contact-points", () -> cassandraContainer.getContactPoint().getAddress());
-        registry.add("spring.data.cassandra.port", () -> cassandraContainer.getMappedPort(9042));
-        registry.add("spring.data.cassandra.local-datacenter", () -> cassandraContainer.getLocalDatacenter());
+        registry.add("spring.cassandra.keyspace-name", () -> "footballKeyspace");
+        registry.add("spring.cassandra.contact-points", () -> cassandraContainer.getContactPoint().getAddress());
+        registry.add("spring.cassandra.port", () -> cassandraContainer.getMappedPort(9042));
+        registry.add("spring.cassandra.local-datacenter", () -> cassandraContainer.getLocalDatacenter());
         registry.add("spring.datasource.url", () -> postgreSQLContainer.getJdbcUrl());
         registry.add("spring.datasource.username", () -> postgreSQLContainer.getUsername());
         registry.add("spring.datasource.password", () -> postgreSQLContainer.getPassword());
