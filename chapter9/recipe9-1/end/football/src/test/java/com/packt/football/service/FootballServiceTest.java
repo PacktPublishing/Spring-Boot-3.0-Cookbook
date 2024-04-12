ea146b7a78a93ac3d8b8d221e0d02f0ce6035f13
felipmiguel <52053185+felipmiguel@users.noreply.github.com>
2024-04-12 22:47:13 +0200

    recipe9-1 fix

--- chapter9/recipe9-1/end/football/src/test/java/com/packt/football/service/FootballServiceTest.java
+++ chapter9/recipe9-1/end/football/src/test/java/com/packt/football/service/FootballServiceTest.java
@@ -20,7 +20,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.dao.InvalidDataAccessResourceUsageException;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
