58b612229b1c571bfa18de8086a1d0107baec8e1
felipmiguel <52053185+felipmiguel@users.noreply.github.com>
2024-04-14 01:17:10 +0200

    recipe9-4 start (jakarta)

---
+++ chapter9/recipe9-4/end/football/src/main/java/com/packt/football/configuration/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.packt.football.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails userAdmin = User.withDefaultPasswordEncoder()
+                .username("packt")
+                .password("packt")
+                .roles("ADMIN")
+                .build();
+        UserDetails simpleUser = User.withDefaultPasswordEncoder()
+                .username("user1")
+                .password("user1")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(userAdmin, simpleUser);
+    }
+
+    @Bean
+    WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .antMatchers("/security/public/**");
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeRequests(authorizeRequests -> {
+                    try {
+                        authorizeRequests
+                                .antMatchers("/").permitAll()
+                                .antMatchers("/security/private/**").hasRole("ADMIN")
+                                .and()
+                                .httpBasic();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }).build();
+    }
+}
--- chapter9/recipe9-4/end/football/src/test/java/com/packt/football/controller/FootballControllerTest.java
+++ chapter9/recipe9-4/end/football/src/test/java/com/packt/football/controller/FootballControllerTest.java
@@ -1,7 +1,10 @@
 package com.packt.football.controller;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.time.LocalDate;
@@ -10,16 +13,17 @@ import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.packt.football.configuration.SecurityConfig;
 import com.packt.football.domain.Player;
 import com.packt.football.service.FootballService;
 
 @WebMvcTest(FootballController.class)
-@AutoConfigureMockMvc
+@Import(SecurityConfig.class)
 class FootballControllerTest {
 
     @Autowired
@@ -35,7 +39,8 @@ class FootballControllerTest {
         given(footballService.getPlayersByMatch(1)).willReturn(players);
 
         mvc.perform(get("/football/matches/1/players"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)));
     }
 
     @Test
@@ -45,70 +50,82 @@ class FootballControllerTest {
         given(footballService.getPlayersByMatch(1)).willReturn(players);
 
         mvc.perform(get("/football/matches/1/players/"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)));
     }
 
     @Test
     void getAlbumTeamPlayers() throws Exception {
-        Player player = new Player(1, "Player 1", 1, "Goalkeeper", LocalDate.of(1990, 1, 1));
+        Player player = new Player(1, "Player 1", 1, "Goalkeeper", LocalDate.of(1990,
+                1, 1));
         List<Player> players = List.of(player);
         given(footballService.getAlbumPlayersByTeam(1, 1)).willReturn(players);
 
         mvc.perform(get("/football/albums/1/1/players"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)));
     }
 
     @Test
     void getAlbumTeamPlayers_withTrailSlash() throws Exception {
-        Player player = new Player(1, "Player 1", 1, "Goalkeeper", LocalDate.of(1990, 1, 1));
+        Player player = new Player(1, "Player 1", 1, "Goalkeeper", LocalDate.of(1990,
+                1, 1));
         List<Player> players = List.of(player);
         given(footballService.getAlbumPlayersByTeam(1, 1)).willReturn(players);
 
         mvc.perform(get("/football/albums/1/1/players/"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)));
     }
 
     @Test
     void getAlbumMissingPlayers() throws Exception {
-        Player player = new Player(1, "Player 1", 1, "Goalkeeper", LocalDate.of(1990, 1, 1));
+        Player player = new Player(1, "Player 1", 1, "Goalkeeper", LocalDate.of(1990,
+                1, 1));
         List<Player> players = List.of(player);
         given(footballService.getAlbumMissingPlayers(1)).willReturn(players);
 
         mvc.perform(get("/football/albums/1/missingplayers"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)));
     }
 
     @Test
     void getAlbumMissingPlayers_withTrailSlash() throws Exception {
-        Player player = new Player(1, "Player 1", 1, "Goalkeeper", LocalDate.of(1990, 1, 1));
+        Player player = new Player(1, "Player 1", 1, "Goalkeeper", LocalDate.of(1990,
+                1, 1));
         List<Player> players = List.of(player);
         given(footballService.getAlbumMissingPlayers(1)).willReturn(players);
 
         mvc.perform(get("/football/albums/1/missingplayers/"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)));
     }
 
     @Test
     void getAlbumMyPlayers() throws Exception {
-        Player player = new Player(1, "Player 1", 1, "Goalkeeper", LocalDate.of(1990, 1, 1));
+        Player player = new Player(1, "Player 1", 1, "Goalkeeper", LocalDate.of(1990,
+                1, 1));
         List<Player> players = List.of(player);
         given(footballService.getAlbumPlayers(1)).willReturn(players);
 
         mvc.perform(get("/football/albums/1/myplayers"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)));
+        
     }
 
     @Test
     void getAlbumMyPlayers_withTrailSlash() throws Exception {
-        Player player = new Player(1, "Player 1", 1, "Goalkeeper", LocalDate.of(1990, 1, 1));
+        Player player = new Player(1, "Player 1", 1, "Goalkeeper", LocalDate.of(1990,
+                1, 1));
         List<Player> players = List.of(player);
         given(footballService.getAlbumPlayers(1)).willReturn(players);
 
-        mvc.perform(get("/football/albums/1/myplayers"))
-                .andExpect(status().isOk());
+        mvc.perform(get("/football/albums/1/myplayers/"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)));
     }
-
-
 
     @Test
     void getMatchWithTimeline() throws Exception {
@@ -131,7 +148,9 @@ class FootballControllerTest {
         given(footballService.getMatchEventsOfType(1, 1)).willReturn(List.of());
 
         mvc.perform(get("/football/matches/1/timeline/events/1"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content().string("[]"));
+        
     }
 
     @Test
@@ -139,7 +158,8 @@ class FootballControllerTest {
         given(footballService.getMatchEventsOfType(1, 1)).willReturn(List.of());
 
         mvc.perform(get("/football/matches/1/timeline/events/1/"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content().string("[]"));
     }
 
     @Test
@@ -147,7 +167,8 @@ class FootballControllerTest {
         given(footballService.getTotalPlayersWithMoreThanNMatches(5)).willReturn(10);
 
         mvc.perform(get("/football/players/matches/5"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content().string("10"));
     }
 
     @Test
@@ -155,7 +176,8 @@ class FootballControllerTest {
         given(footballService.getTotalPlayersWithMoreThanNMatches(5)).willReturn(10);
 
         mvc.perform(get("/football/players/matches/5/"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content().string("10"));
     }
 
 }
