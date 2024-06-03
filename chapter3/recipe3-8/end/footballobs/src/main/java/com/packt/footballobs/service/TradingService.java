a0c9a2b04d6dd693eba4b3788970b619e2015e19
felipmiguel <52053185+felipmiguel@users.noreply.github.com>
2024-06-04 00:09:24 +0200

    recipe 3-8 end

--- chapter3/recipe3-8/end/footballobs/src/main/java/com/packt/footballobs/service/TradingService.java
+++ chapter3/recipe3-8/end/footballobs/src/main/java/com/packt/footballobs/service/TradingService.java
@@ -2,6 +2,8 @@ package com.packt.footballobs.service;
 
 import java.util.Random;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.availability.AvailabilityChangeEvent;
 import org.springframework.boot.availability.LivenessState;
 import org.springframework.context.ApplicationEventPublisher;
@@ -10,6 +12,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class TradingService {
 
+    private static final Logger logger = LoggerFactory.getLogger(TradingService.class);
+
     private ApplicationEventPublisher applicationEventPublisher;
 
     public TradingService(ApplicationEventPublisher applicationEventPublisher) {
@@ -17,15 +21,21 @@ public class TradingService {
     }
 
     public int getPendingOrders() {
+        logger.debug("Ensuring that pending orders can be calculated");
         Random random = new Random();
-        return random.nextInt(100);
+        int pendingOrders = random.nextInt(100);
+        logger.info(pendingOrders + " pending orders found");
+        return pendingOrders;
+
     }
 
     public int tradeCards(int orders) {
         if (getPendingOrders() > 90) {
+            logger.warn("There are more than 90 orders, this can cause the system to crash");
             AvailabilityChangeEvent.publish(applicationEventPublisher,
                     new Exception("There are more than 90 pending orders"), LivenessState.BROKEN);
         } else {
+            logger.debug("There are more less than 90 orders, can manage it");
             AvailabilityChangeEvent.publish(applicationEventPublisher,
                     new Exception("working fine"), LivenessState.CORRECT);
         }
